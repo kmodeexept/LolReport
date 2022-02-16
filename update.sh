24c0ef2b19fab8f4142ba014e9992e3af44a15f2
Marius Schlueter <themrslue@googlemail.com>
2022-02-16 14:22:11 +0100

    added otherfiles to update script

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 zip -g package.zip lambda_function.py
+zip -g package.zip template_website.html
+zip -g package.zip api.key 
 aws lambda update-function-code --function-name webSite --zip-file fileb://function.zip
