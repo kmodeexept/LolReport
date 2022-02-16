8ae591f340e1c55ab61b33283cfede6b419bb354
Marius Schlueter <themrslue@googlemail.com>
2022-02-16 18:58:16 +0100

    added automatic initialization and fixed bug

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python3 -m venv venv
-cd venv/lib/python3*/site-package/
-zip -r package.zip *
-cd ../../../../
+pythonver=$(ls venv/lib)
+cd venv/lib/$pythonver/site-packages && zip -r ../../../../package.zip *
--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-zip -g package.zip lambda_function.py
-zip -g package.zip template_website.html
-zip -g package.zip api.key 
-aws lambda update-function-code --function-name webSite --zip-file fileb://package.zip > /dev/null
+FILE=package.zip
+
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "create venv and package"
+    ./configure.sh
+    echo "configure finished"
+fi
+
+zip -g $FILE lambda_function.py
+zip -g $FILE template_website.html
+zip -g $FILE api.key 
+aws lambda update-function-code --function-name webSite --zip-file fileb://$FILE > /dev/null
